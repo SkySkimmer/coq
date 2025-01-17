b61861dd82f9921bed177a152ac893c65bfd4caf
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2025-01-17 16:35:28 +0100

    Overlays for template pseudo sort poly

---
+++ dev/ci/user-overlays/19985-SkySkimmer-cominductive-lbound.sh
@@ -0,0 +1,7 @@
+overlay elpi https://github.com/SkySkimmer/coq-elpi cominductive-lbound 19985
+
+overlay coq_lsp https://github.com/SkySkimmer/coq-lsp cominductive-lbound 19985
+
+overlay paramcoq https://github.com/SkySkimmer/paramcoq cominductive-lbound 19985
+
+overlay lean_importer https://github.com/SkySkimmer/coq-lean-import cominductive-lbound 19985
